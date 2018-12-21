069e856f50d6be0d9813781732363e80cc6ab50e
huangxianshuai <huangxianshuai@360.cn>
2018-12-21 13:38:39 +0800

    initial

---
+++ QiDragSortView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QiDragSortView
+//
+//  Created by huangxianshuai on 2018/12/21.
+//  Copyright © 2018年 QiShare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QiDragSortView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  QiDragSortView
+//
+//  Created by huangxianshuai on 2018/12/21.
+//  Copyright © 2018年 QiShare. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
